883fd830ca72301b58b0db44496430c7ae93b009
caiquebslima <caiquebslima@gmail.com>
2022-06-15 06:29:08 -0300

    feat: setting editor config feat: creating first route

--- backend/server.ts
+++ backend/server.ts
@@ -1,1 +1,12 @@
-console.log('hello world 2')
+import { Router, Request, Response } from "express";
+import express from "express";
+const dotenv = require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.get("/api/goals", (req: Request, res: Response) => {
+    res.status(200).json({ message: "Hello World" });
+});
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
